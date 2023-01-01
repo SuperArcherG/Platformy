3e24948a5446719d444c845e50c30c3a64de96d9
SuperArcherG <JudahBertou@gmail.com>
2023-01-01 03:51:46 -0500

    e

--- BuildMac.sh
+++ BuildMac.sh
@@ -1,4 +1,4 @@
-pyinstaller Platformy.py -F -c --clean -y --target-architecture x86_64 -i assets/images/icon.icns
+pyinstaller Platformy.py -F -w --clean -y --target-architecture arm64 -i icon.icns --debu all
 rm Platformy.spec
 rm -r build
 cd dist
